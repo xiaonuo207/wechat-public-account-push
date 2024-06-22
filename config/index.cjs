39189a7ae5398f6bcf17e5b167f9a3ab3941f9b9
xiaonuo207 <895809629@qq.com>
2024-06-22 21:06:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqrz96fyKzVaikCPDPXcGbG_AAZw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'VXMqAnNmAmlFPTI9xXSw8RmqUf3H4gr7OrWHFz0YEMI',
+      useTemplateId: 'DZfHWkYBdbFYPxXSSd5dV8k370duNlWPFC2EVKOB_vg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-29',
       festivals: [
@@ -50,7 +50,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'VXMqAnNmAmlFPTI9xXSw8RmqUf3H4gr7OrWHFz0YEMI',
+  CALLBACK_TEMPLATE_ID: 'DZfHWkYBdbFYPxXSSd5dV8k370duNlWPFC2EVKOB_vg',
 
   CALLBACK_USERS: [
     {
