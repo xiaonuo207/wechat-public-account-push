f7bc9cbb6e32dbb6bb05397946f335b1f5c0dabd
xiaonuo207 <895809629@qq.com>
2024-06-22 21:49:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '兔兔媳妇',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oqrz96fyKzVaikCPDPXcGbG_AAZw',
+      id: 'oqrz96RmN20uvLaw0xfa2nRqI6Fw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'DZfHWkYBdbFYPxXSSd5dV8k370duNlWPFC2EVKOB_vg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
